e8346f0383b632e0891cb7f3efd07ba8ae30a1bd
Angelina web2025-1 <apavlushce@gmail.com>
2026-05-12 16:38:56 +0300

    feat: налаштування основних маршрутів (Home та Booking)

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Booking from './pages/Booking';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/booking/:trainId" element={<Booking />} /> 
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
